350bed21a5e116d9025434fe5a03897867392cf0
Jackson Camp <jacksoncamp42@gmail.com>
2022-06-25 08:58:04 -0700

    Finished Exercises 2.19 - 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,11 @@ import Persons from "./components/Person";
 import Filter from "./components/Filter";
 import AddNew from "./components/AddNew";
 import personService from "./services/person";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [persons2, setPersons2] = useState([]);
+  const [message, setMessage] = useState(null);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -50,13 +51,25 @@ const App = () => {
         const newPerson = { ...oldInfo, number: newNumber };
         personService
           .update(oldInfo.id, newPerson)
-          .then((data) =>
+          .then((data) => {
             setPersons(
               persons
                 .filter((person) => person.id !== newPerson.id)
                 .concat(data)
-            )
-          );
+            );
+            setMessage(`${newName} was successfully updated`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            console.log(error);
+            setPersons(persons.filter((person) => person.id !== newPerson.id));
+            setMessage(`[ERROR] ${newName} was already deleted from server`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          });
       }
     } else {
       if (persons.length === 0) {
@@ -67,7 +80,20 @@ const App = () => {
         };
         personService
           .create(newPerson)
-          .then((initialData) => setPersons(persons.concat(initialData)));
+          .then((initialData) => {
+            setPersons(persons.concat(initialData));
+            setMessage(`${newName} was successfully added`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            setMessage(`[ERROR] ${error.response.data.error}`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+            console.log(error.response.data);
+          });
       } else {
         const newPerson = {
           name: newName,
@@ -76,8 +102,22 @@ const App = () => {
         };
         personService
           .create(newPerson)
-          .then((initialData) => setPersons(persons.concat(initialData)));
+          .then((initialData) => {
+            setPersons(persons.concat(initialData));
+            setMessage(`${newName} was successfully added`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+          })
+          .catch((error) => {
+            setMessage(`[ERROR] ${error.response.data.error}`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+            console.log(error.response.data);
+          });
       }
+      setMessage(`Added ${newName} to phonebook`);
     }
     setNewName("");
     setNewNumber("");
@@ -91,6 +131,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <AddNew
         newName={newName}
